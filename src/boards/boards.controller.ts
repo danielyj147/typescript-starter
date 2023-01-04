7d221a83d9f6769b27813ba6a1403926cf52a0ac
Daniel Jeong <danielyj147@gmail.com>
2023-01-04 19:41:00 +0900

    added CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -12,10 +13,25 @@ export class BoardsController {
 
   @Post()
   createBoard(
-    @Body('title') title: string,
-    @Body('description') description: string,
+    // !!! Body in this context means the HTTP request body, which is in JSON format
+    // Try postman to test this Post method.
+    @Body() createBoardDto: CreateBoardDto,
   ): Board {
     // @Body('title') ==> title, @Body('description') ==> description
-    return this.boardsService.createBoard(title, description);
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  // The request that we will send: http://localhost:3002/boards/?id=f519a0a0-8c16-11ed-a5c0-7f2d35791055
+  // !!! if you have multitple params, then use "&" in your request url
+  // ex) localhost:3002/boards/?id=12&title=134
+  // !!! For multiple params, just use "@Param() params: string[]" as opposed to "@Param('id') id: string", no string inside ()
+  @Get('/:id')
+  getBoardById(@Param('id') id: string) {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 // provider - services get imported to a controller
 // process or get data then gives it to controller
@@ -8,20 +9,32 @@ import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
+  // What gets returend -- incremented by createBoard
   private boards: Board[] = [];
 
   getAllBoards(): Board[] {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(), // creating unique id's
-      title,
+      // ...createBoardDto, //alternative
+      title, // !!! no need to write title: title, becasue both have the same name
       description,
       status: BoardStatus.PUBLIC,
     };
     this.boards.push(board);
     return board;
+  }
+
+  // for Get() operation
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+  // for Delete() operation
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
   }
 }
